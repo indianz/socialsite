5d019ce932c93f2b6fb3882c1073740d6e029122
Kiru Samapathy <ksama@thoughtworks.com>
2016-09-03 17:52:26 +1000

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-08-28 04:03:40 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-08-28T04:03:40+00:00	INFO ::1	joomlafailure	Empty password not allowed.
